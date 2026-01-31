5e0ada958e8a112538722bf1340055f3165953ce
jigglebaby <gokukakarat92@gmail.com>
2026-01-31 13:08:12 +0000

    new file

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-def name(input_name):   
-  print(f"your name is: {input_name}")
-  return input_name.lower()
 
+from random import choice
+from utils import add, is_even, c_to_f, roll_die
 
-n = input("enter your name: ")
-x = name(n)
+while True:
+  print('\n--- Utility Toolkit ---')
+  print('1) Add two numbers')
+  print('2) Even or Odd')
+  print('3) Celsius to Farenheit')
+  print('4) Roll a die')
+  print('5) Exit')
 
-file_w = open("save.txt", "w")
-file_w.write(x)
-file_w.close()
+  choice = input('Choose: ')
 
-file_r = open("hello.txt", "r")
-print(file_r.read())
-file_r.close()
+  if choice == '1':
+    a = float(input('Enter a: '))
+    b = float(input('Enter b: '))
+    print ('Results:', add(a, b))
+  elif choice == '2':
+    n = int(input('Enter a number: '))
+    print('Even' if is_even(n) else 'Odd')
+  elif choice == '3':
+    c = float(input('Celsius: '))
+    print('Farenheight:', c_to_f(c))
+  elif choice == '4':
+   print('you rolled:', roll_die())
+  elif choice == '5':
+    print('Bye!')
+    break
+  else:
+    print('Invalid Chocie')
+  
---
+++ utils.py
@@ -0,0 +1,14 @@
+import random
+
+def add(a, b):
+  return a + b
+
+def is_even(n):
+  return n % 2 == 0
+
+def c_to_f(c):
+  return (c*9/5) + 32
+
+def roll_die():
+  return random.randint(1, 6)
+  
